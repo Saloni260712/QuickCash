c97eabd85e79620373c3ddee6107fadfe4e03211
Saloni260712 <patelsaloni.0711@gmail.com>
2025-02-14 23:58:34 -0500

    Initial Commit

---
+++ QuickCash/QuickCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCashApp.swift
+//  QuickCash
+//
+//  Created by Meet Patel on 14/02/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
